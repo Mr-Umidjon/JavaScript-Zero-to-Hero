3c48e153c462e60cb7b1f056cb0e152f385a4c39
Umidjon <mrumidjon2020@gmail.com>
2022-10-30 20:41:36 +0500

    data tyeps

--- 07-data-types/js/script.js
+++ 07-data-types/js/script.js
@@ -23,4 +23,32 @@ console.log(isMarried);
 console.log(isCircleEarth);
 
 
+// null
+// console.log(clientAge);
 
+
+// undefined
+let und;
+
+console.log(und); // undefined
+
+
+// Object
+const thief = {
+//  key: value
+    age: 30, // number
+    jacket: 'black', // string
+    isLong: false, // boolean
+};
+
+console.log(thief.jacket); // thief['jacket']
+console.log(thief.age); // thief['age']
+console.log(thief.isLong); // thief['isLong']
+console.log(thief);
+
+
+// array
+const colors = ['red', 'black', 'green'];
+
+console.log(colors);
+console.log(colors[0]);
