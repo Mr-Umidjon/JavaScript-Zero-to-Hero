f674cc66e8e4fba0ea06f85613473ce5c6655484
Umidjon <mrumidjon2020@gmail.com>
2022-10-30 22:26:32 +0500

    interpolation

---
+++ 09-interpolation/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const channel = prompt("What's your favourite youtuber?");
+
+console.log("https://youtube.com/" + channel + "/videos");
+console.log(`https://youtube.com/${channel}/videos`);
+
+
+const user = "Umidjon";
+const job = "JavaScript developer";
+
+console.log(`User's name is ${user}, he is ${job}`);
