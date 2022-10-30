783f93955df14d4c6073e13344d29547d2397f17
Umidjon <mrumidjon2020@gmail.com>
2022-10-30 20:39:06 +0500

    data tyeps

---
+++ 07-data-types/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Number
+let number = 10.4; // Data Type = Number
+
+console.log(8 / 0); // Infinity
+console.log(-8 / 0); // - Infinity
+
+console.log("Samar" * 8); // NaN
+
+
+// String
+const clientName = "Umidjon"; // `Umidjon`
+console.log(clientName);
+
+
+// Boolean
+const isMarried = false;
+const isCircleEarth = true;
+// true = yes
+// false = no
+console.log(isMarried);
+console.log(isCircleEarth);
+
+
+
