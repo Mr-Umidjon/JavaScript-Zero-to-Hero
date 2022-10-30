dfa3036e8b82d7f91b8b7d7aa41d723d8a28dbad
Umidjon <mrumidjon2020@gmail.com>
2022-10-30 11:08:54 +0500

    variables

---
+++ 05-variables/js/script.js
@@ -0,0 +1,20 @@
+let number = 12;
+const borderColor = 'red';
+
+console.log(number);
+
+number = 20;
+
+console.log(number);
+
+const person = {
+    name: "Umidjon",
+};
+
+person.name = "Odiljon";
+
+console.log(person);
+
+// Old versions
+var clientName = "Umidjon";
+console.log(clientName);
