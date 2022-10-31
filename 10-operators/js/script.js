25e09f759618e998cbe59bd05d0d47d5c4fa2148
Umidjon <mrumidjon2020@gmail.com>
2022-10-31 19:00:13 +0500

    operators

---
+++ 10-operators/js/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// operators 
+console.log("Umidjon" + "Maxammadsoliyev");
+console.log(6 + "Maxammadsoliyev");
+// console.log(6 +  +'9');
+
+
+//incr, decr
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+console.log(incr++);
+console.log(decr--);
+
+
+console.log(incr);
+console.log(decr);
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+console.log(15 % 4);
+
+console.log(5 * 5 == 25);
+console.log(5 * 5 == "25");
+console.log(5 * 5 === "25");
+
+
+// && , ||
+const isAge = true;
+const isClose = false;
+const isChecked = false;
+
+console.log(isAge && isClose);
+console.log(isAge || isClose);
+console.log(isAge || isClose || isChecked);
+console.log(!isAge);
+
+console.log(2 + 2 * 2 === 8);
+console.log(2 + 2 * 2 !== 8);
