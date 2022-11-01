ddf3d0680b221228855bbed0b416762fcb6c54ba
Umidjon <mrumidjon2020@gmail.com>
2022-11-01 18:25:13 +0500

    if and switch case

---
+++ 13-conditional-statements/js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const age = +prompt("How old are you?", '7');
+
+if (age > 18) {
+    console.log("Welcome!");
+} else {
+    console.log("No entry");
+}
+
+(age > 18) ? console.log("Welcome!") : console.log("No entry");
+
+if (age > 25) {
+    console.log("Horror films");
+} else if (age > 18) {
+    console.log("Boyevik films");
+} else {
+    console.log("Cartoon");
+}
+
+const color = 'red';
+
+switch (color) {
+    case 'red': 
+        console.log("Stop");
+        break;
+    case "green":
+        console.log("Go!");
+        break;
+    case "yellow":
+        console.log("Wait");
+        break;
+    default:
+        console.log("Traffic jam");
+        break;
+}
