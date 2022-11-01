0bb5dba0d03bf6b170c70408db101692fe33dfb3
Umidjon <mrumidjon2020@gmail.com>
2022-11-01 14:12:07 +0500

    practice

---
+++ 12-practice/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const numberOfSeries = +prompt("How many series have you watched?", "0");
+
+const seriesDB = {
+    count: numberOfSeries,
+    series: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const lastSeries1 = prompt("Which series did you last watch?");
+const score1 = +prompt("How many points did you score?", "5");
+
+const lastSeries2 = prompt("Which series did you last watch?");
+const score2 = +prompt("How many points did you score?", "5");
+
+seriesDB.series[lastSeries1] = score1;
+seriesDB.series[lastSeries2] = score2;
+
+console.log(seriesDB);
+
