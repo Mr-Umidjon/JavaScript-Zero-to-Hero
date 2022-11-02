09a09e52b40260046a3db5bfb40851ba4fc16b50
Umidjon <mrumidjon2020@gmail.com>
2022-11-02 14:51:08 +0500

    mathod and property

---
+++ 17-mathods-and-properties/js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const user = "Umidjon";
+
+console.log(user.length);
+
+const arr = [1, 2, 3, 4];
+
+console.log(arr.length);
+
+console.log(user[2]);
+
+// console.log((user[2] = 'r'));
+console.log(user);
+
+console.log(user.toUpperCase());
+const upperCaseUser = user.toUpperCase();
+
+console.log(upperCaseUser);
+
+console.log(user.toLowerCase());
+
+
+
+const greeting = "Hello World!";
+
+console.log(greeting.indexOf("W"));
+console.log(greeting.indexOf("a"));
+
+console.log(greeting.slice(6, 12));
+console.log(greeting.slice(6));
+console.log(greeting.substring(6, 12));
+console.log(greeting.substr(6, 3));
+
+
+const num = 8.8;
+console.log(Math.round(num));
+
+const borderWidth = "13.8px";
+console.log(parseInt(borderWidth));
+console.log(parseFloat(borderWidth));
+
+
