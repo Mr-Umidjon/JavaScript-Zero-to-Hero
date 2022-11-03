a026f1aa560ea885efd57a934d03c7217ce46b5b
Umidjon <mrumidjon2020@gmail.com>
2022-11-03 14:25:07 +0500

    callback functions

---
+++ 19-callback-functions/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// CallBack fubctions
+
+function first(){
+    // code
+    setTimeout(() => {
+        console.log(1);
+    }, 1000);
+}
+
+function second() {
+    // code
+    console.log(2);
+}
+
+first();
+second();
