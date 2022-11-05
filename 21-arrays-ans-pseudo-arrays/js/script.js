27f5d25c47cdbf0520e953afc082f5c56789ceff
Umidjon <mrumidjon2020@gmail.com>
2022-11-05 20:19:17 +0500

    array and pseudo-arrays

--- 21-arrays-ans-pseudo-arrays/js/script.js
+++ 21-arrays-ans-pseudo-arrays/js/script.js
@@ -11,3 +11,19 @@ console.log(arr);
 arr.shift();
 arr.unshift(0);
 console.log(arr);
+
+
+for (let i=0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+for (let value of arr){
+    console.log(value);
+}
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr1);
+
+arr1[99] = 12;
+console.log(arr1.length);
+console.log(arr1);
