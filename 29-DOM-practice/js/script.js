5ef6d6214abc1065471e90de0ddb36be8c233387
Umidjon <mrumidjon2020@gmail.com>
2022-11-11 23:58:02 +0500

    DOM practice

--- 29-DOM-practice/js/script.js
+++ 29-DOM-practice/js/script.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
 const adv = document.querySelectorAll('.promo__adv img'),
   wrapper = document.querySelector('.promo__bg'),
   genre = wrapper.querySelector('.promo__genre'),
-  seriesList = document.querySelector('.promo__interactive-list')
+  seriesList = document.querySelector('.promo__interactive-list');
 
 const seriesDB = {
   series: [
@@ -13,5 +13,26 @@ const seriesDB = {
     'Magnificent Century',
     'The Great Seljuks: Guardians...',
   ],
+};
+
+// console.log(adv);
+adv.forEach(item => {
+  item.remove();
 }
+);
+
+genre.textContent = "Comedy";
+
+wrapper.style.backgroundImage = 'url("img/1.jpg")';
+
+seriesList.innerHTML = '';
+
+seriesDB.series.forEach((item, index) => {
+  seriesList.innerHTML += `
+  <li class="promo__interactive-item">
+    ${index+1} ${item}
+  <div class="delete"></div>
+  </li>
+  `;
+});
 
