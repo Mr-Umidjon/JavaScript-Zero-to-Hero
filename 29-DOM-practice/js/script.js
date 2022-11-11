b61766955447f8127cc0934a49f85930afe037b1
Umidjon <mrumidjon2020@gmail.com>
2022-11-11 23:37:01 +0500

    DOM practice

---
+++ 29-DOM-practice/js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const adv = document.querySelectorAll('.promo__adv img'),
+  wrapper = document.querySelector('.promo__bg'),
+  genre = wrapper.querySelector('.promo__genre'),
+  seriesList = document.querySelector('.promo__interactive-list')
+
+const seriesDB = {
+  series: [
+    'Omar',
+    'The Final Legacy',
+    'Ertugrul',
+    'Magnificent Century',
+    'The Great Seljuks: Guardians...',
+  ],
+}
+
